f1a6aca58219f431b83a47a5c860b1d30b8e45cb
SrKaique <100378618+kaiquemm7@users.noreply.github.com>
2026-07-08 20:34:24 -0300

    Update local API_URL for development environment

--- docs/js/config.js
+++ docs/js/config.js
@@ -7,5 +7,5 @@
 // const API_URL = "https://modelo-almoxarifado-backend.onrender.com/api";
 
 const API_URL = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "http://localhost:10000/api"
+  ? "http://localhost:3001/api"
   : "https://qualquercoisa2066.onrender.com/api";
